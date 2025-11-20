f8dc427ae3dfaf4ff502db74befce57db77d5a97
sofielucia <sofieluciag@gmail.com>
2025-11-20 19:45:34 +0100

    Byttet om på indhold i int og string, så de passende værdier

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        User u1 = new User("Bob", 10);
-        User u2 = new User("Alice", 20);
-        User u3 = new User("Carl", 30);
+        User u1 = new User(10, "Bob");
+        User u2 = new User(20, "Alice");
+        User u3 = new User(30, "Carl");
 
         UserList list = new UserList();
         list.add(u1);
         list.add(u2);
         list.add(u3);
 
-        System.out.println(list.getu(new User("Bob", 11)));
+        System.out.println(list.getu(new User(11, "Bob")));
 
         Persister.s(list);
     }
