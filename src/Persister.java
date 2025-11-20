8656bede719d5cc77012ca6a5c961258f41fc68e
sofielucia <sofieluciag@gmail.com>
2025-11-20 20:04:28 +0100

    Forståelighed. Fjernet overflødige kommentarer

--- src/Persister.java
+++ src/Persister.java
@@ -9,10 +9,10 @@ public class Persister
         FileWriter fw = new FileWriter(file); // Create filewriter
         BufferedWriter bw = new BufferedWriter(fw); // Create BufferedWriter
         for(int x=0;x<u.size()-1;x++) // Create a loop
-        { // Nicely formatted block
+        {
             bw.write(u.get(x).c_nam); // Write first thing
             bw.write(u.get(x).geti()); // Write second thing
-        } // Ending parenthesis
+        }
     }
 
     public static void s(UserList list) {
