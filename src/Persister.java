e7013610e656b402ea3242a307f4cc2e0d67bde9
sofielucia <sofieluciag@gmail.com>
2025-11-20 20:13:24 +0100

    Forståelighed. ubrugt variabel

--- src/Persister.java
+++ src/Persister.java
@@ -1,19 +1,5 @@
-import java.io.*;
-import java.util.*;
-
 public class Persister
 {
-    public static void s(ArrayList<User> u) throws IOException
-    {
-        File file = new File("users.txt"); // Creates file
-        FileWriter fw = new FileWriter(file); // Create filewriter
-        BufferedWriter bw = new BufferedWriter(fw); // Create BufferedWriter
-        for(int x=0;x<u.size()-1;x++) // Create a loop
-        {
-            bw.write(u.get(x).c_nam); // Write first thing
-            bw.write(u.get(x).geti()); // Write second thing
-        }
-    }
 
     public static void s(UserList list) {
     }
